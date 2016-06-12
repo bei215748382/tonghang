a6fd2f3cc61f7c6dfc9586f4f39f90ab67d29d6b
yxx <yxx@yxx-SVF1431AYCW>
2016-06-12 23:21:23 +0800

    提交文件上传

--- tonghang/src/main/java/com/tonghang/server/util/OSSUtil.java
+++ tonghang/src/main/java/com/tonghang/server/util/OSSUtil.java
@@ -2,45 +2,51 @@ package com.tonghang.server.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.ObjectMetadata;
 
 public class OSSUtil {
-    private String host = "http://oss-cn-shanghai.aliyuncs.com";
-    private OSSClient client =
-            new OSSClient(host, "PlQXvaWcQFfnraoC", "yHr0DLbxZ71BCu4KHADhGUKdZGMUMA");
-    private String VideoBucket = "tonghangimg";
-    private String videoPath = "http://tonghangimg.oss-cn-shanghai.aliyuncs.com";
-    private static OSSUtil ossUtil;
+	private String host = "http://oss-cn-shanghai.aliyuncs.com";
+	private OSSClient client = new OSSClient(host, "PlQXvaWcQFfnraoC", "yHr0DLbxZ71BCu4KHADhGUKdZGMUMA");
+	private String VideoBucket = "tonghangimg";
+	private String videoPath = "http://tonghangimg.oss-cn-shanghai.aliyuncs.com";
+	private static OSSUtil ossUtil;
 
-    private OSSUtil() {
+	private OSSUtil() {
 
-    }
+	}
 
-    public static OSSUtil instance() {
-        if (ossUtil == null) {
-            ossUtil = new OSSUtil();
-        }
-        return ossUtil;
-    }
+	public static OSSUtil instance() {
+		if (ossUtil == null) {
+			ossUtil = new OSSUtil();
+		}
+		return ossUtil;
+	}
 
-    public String uploadOss(String filepath) throws IOException {
+	public String uploadOss(String filepath) throws IOException {
 
-        File videofile = new File(filepath);
+		File videofile = new File(filepath);
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(videofile.getTotalSpace());
-        client.putObject(VideoBucket, videofile.getPath(), videofile, metadata);
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(videofile.getTotalSpace());
+		client.putObject(VideoBucket, videofile.getPath(), videofile, metadata);
 
-        videofile.delete();
+		videofile.delete();
 
-        return videoPath + filepath;
-    }
-    
-    public static void main(String[] args) throws IOException {
-        
-        OSSUtil.instance().uploadOss("G://nginx.conf");
-    }
+		return videoPath + filepath;
+	}
+
+	public String uploadOss(InputStream inputstream, ObjectMetadata metadata, String path) throws IOException {
+
+		client.putObject(VideoBucket, path, inputstream, metadata);
+		return videoPath + path;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		OSSUtil.instance().uploadOss("G://nginx.conf");
+	}
 
 }
